4c5d02553d4e3e2971b639eb64da47d3d1d6e320
unknown <hyunmi.kim@utec.edu.pe>
2019-09-11 17:07:04 -0500

    11 de septiembre

--- session4a/main.cpp
+++ session4a/main.cpp
@@ -2,5 +2,9 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int ival = 42;
+    int *pival = &ival;
+    std::cout << pival << "\t"  << *pival << "\t" << &pival;
     return 0;
+
 }
