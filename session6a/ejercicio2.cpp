f576e68e5f60002ee345458d719301d70a5aa163
unknown <hyunmi.kim@utec.edu.pe>
2019-09-25 17:11:11 -0500

    session6a

---
+++ session6a/ejercicio2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout<<"Ejercicio2"<<endl;
+    int *a = new int [10];
+    a[0] = 1;
+    a[1] = 19;
+    a[9] = 3;
+    cout << "index" <<"\t" << "Value"<<"\t Mem adress"<<endl;
+    for(int i=0; i < 10; i++) {
+        cout<<i<<"\t"<<a[i]<<"\t"<<&a[i]<<endl;
+    }
+    delete []a;
+    return 0;
+}
