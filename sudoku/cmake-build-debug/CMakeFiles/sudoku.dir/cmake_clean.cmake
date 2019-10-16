8edd04fb51425732166e33c1bfba63689a1287aa
StrikeRose <40776002+StrikeRose@users.noreply.github.com>
2019-10-16 00:05:11 -0500

    Add files via upload

---
+++ sudoku/cmake-build-debug/CMakeFiles/sudoku.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sudoku.dir/main.cpp.obj"
+  "sudoku.pdb"
+  "sudoku.exe"
+  "sudoku.exe.manifest"
+  "libsudoku.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sudoku.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
